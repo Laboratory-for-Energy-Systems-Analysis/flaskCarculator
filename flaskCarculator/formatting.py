1dca4a5c3a105dfd45206b924dbc03684d61a38d
romainsacchi <romain@Romains-iMac.psi.ch>
2025-01-17 13:42:03 +0100

    Fix import of __version__

--- flaskCarculator/formatting.py
+++ flaskCarculator/formatting.py
@@ -62,8 +62,9 @@ def format_results_for_tcs(data: xr.DataArray, params: dict) -> dict:
     # climate change results need to be in grams CO2-eq., not kg
     data.results.loc[dict(impact_category="climate change")] *= 1000
 
+    # direct exhaust emission is passed directly. it should override the value calculated.
     if "direct_co2" in params:
-        data.results.loc[dict(impact_category="climate change", impact="energy chain")] = params["direct_co2"]
+        data.results.loc[dict(impact_category="climate change", impact="direct - exhaust")] = params["direct_co2"]
 
     for field, subfield in TCS_IMPACTS_MAPPING.items():
         results[field] = data.results.sel(
