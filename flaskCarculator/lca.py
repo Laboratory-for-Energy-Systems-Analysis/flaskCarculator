662f0575d77143f109fa615f82f75a45f15814b1
romainsacchi <romain@Romains-iMac.psi.ch>
2025-09-03 17:58:38 +0200

    fixed scores sheet

--- flaskCarculator/lca.py
+++ flaskCarculator/lca.py
@@ -348,8 +348,8 @@ def redimension_battery_and_range(m, params):
             m[var] = m["electric energy stored"] * 3600 / m["TtW energy, electric mode"]
             m.set_vehicle_masses()
             m.override_battery_capacity()
-            m.calculate_ttw_energy()
-            m.drop_hybrid()
+            #m.calculate_ttw_energy()
+            #m.drop_hybrid()
 
             print(f"After Redimensioning battery: {m['electric energy stored'].values} kWh")
 
@@ -523,6 +523,8 @@ def initialize_model(params, nomenclature=None):
         m = set_vehicle_properties_after_run(m, params)
     print(f"Electric energy stored just AFTER set_vehicle_properties_after_run(): {m['electric energy stored'].values} kWh")
 
+    if "capacity" in m.energy_storage:
+        m.override_battery_capacity()
     # redimension_battery_and_range(m, params)
 
     errors = validate_output_data(data=m, request=params, nomenclature=nomenclature)
